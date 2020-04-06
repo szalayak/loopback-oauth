085cef93ec87474aa8716b6905a43a44fab4cc03
Akos Szalay <akos.szalay@itelligencegroup.co.uk>
2020-04-06 09:02:57 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {LoopbackOauthApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {LoopbackOauthApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new LoopbackOauthApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
