029c8e4c54a4bbdec1e94d657179432396a411c1
Akos Szalay <akos.szalay@itelligencegroup.co.uk>
2020-05-11 18:52:19 +0100

    reverting changes to sequence

--- src/sequence.ts
+++ src/sequence.ts
@@ -1,10 +1,4 @@
-import {
-  AuthenticateFn,
-  AuthenticationBindings,
-  AUTHENTICATION_STRATEGY_NOT_FOUND,
-  USER_PROFILE_NOT_FOUND,
-} from '@loopback/authentication';
-import { inject } from '@loopback/context';
+import {inject} from '@loopback/context';
 import {
   FindRoute,
   InvokeMethod,
@@ -21,61 +15,21 @@ const SequenceActions = RestBindings.SequenceActions;
 export class MySequence implements SequenceHandler {
   constructor(
     @inject(SequenceActions.FIND_ROUTE) protected findRoute: FindRoute,
-    @inject(SequenceActions.PARSE_PARAMS)
-    protected parseParams: ParseParams,
+    @inject(SequenceActions.PARSE_PARAMS) protected parseParams: ParseParams,
     @inject(SequenceActions.INVOKE_METHOD) protected invoke: InvokeMethod,
-    @inject(SequenceActions.SEND) protected send: Send,
-    @inject(SequenceActions.REJECT) protected reject: Reject,
-    @inject(AuthenticationBindings.AUTH_ACTION)
-    protected authenticateRequest: AuthenticateFn,
-  ) { }
+    @inject(SequenceActions.SEND) public send: Send,
+    @inject(SequenceActions.REJECT) public reject: Reject,
+  ) {}
 
   async handle(context: RequestContext) {
     try {
-      const { request, response } = context;
+      const {request, response} = context;
       const route = this.findRoute(request);
-
-      // usually authentication is done before proceeding to parse params
-      // but in our case we need the path params to know the provider name
       const args = await this.parseParams(request, route);
-
-      // if provider name is available in the request path params, set it in the query
-      if (route.pathParams && route.pathParams.provider) {
-        request.query['oauth2-provider-name'] = route.pathParams.provider;
-      }
-
-      //call authentication action
-      await this.authenticateRequest(request);
-
-      // Authentication successful, proceed to invoke controller
       const result = await this.invoke(route, args);
       this.send(response, result);
-    } catch (error) {
-      /**
-       * Authentication errors for login page are handled by the express app
-       */
-      if (
-        context.request.path === '/login' &&
-        (error.status === 401 || error.name === 'UnauthorizedError')
-      ) {
-        /**
-         * The express app that routed the /signup call to LB App, will handle the error event.
-         */
-        context.response.emit(
-          'UnauthorizedError',
-          'User Authentication Failed',
-        );
-        return;
-      }
-
-      if (
-        error.code === AUTHENTICATION_STRATEGY_NOT_FOUND ||
-        error.code === USER_PROFILE_NOT_FOUND
-      ) {
-        Object.assign(error, { statusCode: 401 /* Unauthorized */ });
-      }
-      this.reject(context, error);
-      return;
+    } catch (err) {
+      this.reject(context, err);
     }
   }
 }
