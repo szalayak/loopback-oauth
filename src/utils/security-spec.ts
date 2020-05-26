e339fbb6a287fe5e2bf9d85aae4f4b5669fc60ce
Akos Szalay <akos.szalay@itelligencegroup.co.uk>
2020-05-26 07:41:33 +0100

    security spec

---
+++ src/utils/security-spec.ts
@@ -0,0 +1,18 @@
+import {SecuritySchemeObject, ReferenceObject} from '@loopback/openapi-v3';
+
+export const OPERATION_SECURITY_SPEC = [{oAuth2Auth: []}];
+export type SecuritySchemeObjects = {
+  [securityScheme: string]: SecuritySchemeObject | ReferenceObject;
+};
+export const SECURITY_SCHEME_SPEC: SecuritySchemeObjects = {
+  oAuth2: {
+    type: 'oauth2',
+    flows: {
+      authorizationCode: {
+        authorizationUrl: '/oauth/authorize',
+        tokenUrl: '/oauth/token',
+        scopes: {'*': 'Full Access'},
+      },
+    },
+  },
+};
